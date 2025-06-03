95528f45f3f9979531cd27a44a7ccbbbd0e5bee0
think book <li8399430@gmail.com>
2025-06-03 16:06:16 +0800

    '移除'

--- src/config/ALL_KEYS.py
+++ src/config/ALL_KEYS.py
@@ -1,7 +1,3 @@
-hkust_openai_key ="1CopZeypHcpbuerDSNGYT8ryNJE3izi3Uu837aa7d2404c7baebac9d080af213b"
-hkust_openai_base_url = "https://gpt-api.hkust-gz.edu.cn/v1/chat/completions"
-Authorization_hkust_key = "3259cd78a8594fa08a8e186bc2fcaac851837aa7d2404c7baebac9d080af213b"
-
 common_openai_key = "2e2308bb-976a-4ea7-badd-0ab1bf2066bd"
 common_openai_base_url = "https://api-inference.modelscope.cn/v1/"
 
